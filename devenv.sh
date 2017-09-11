7c1b16910acfa7216b8ee9b50560050b896f350d
Jeff <writetojeffrey@gmail.com>
2017-09-11 01:17:08 -0700

    Add nexar to devenv.sh

--- devenv.sh
+++ devenv.sh
@@ -1,7 +1,16 @@
 source ~/use_ros.sh
 source ~/use_miniconda2.sh
+if [ "$1" = "capstone" ]
+then
+    source ~/repo/CarND-Capstone/ros/devel/setup.sh
+    source activate capstone
+    cd ~/repo/CarND-Capstone
+fi
+if [ "$1" = "nexar" ]
+then
+    source activate nexar
+    cd ~/repo/nexar
+fi
 # source activate didi2
 # source activate squeezeDet
-source activate capstone
 # export PYTHONPATH=$PYTHONPATH:/home/eljefec/repo/squeezeDet/src:/home/eljefec/repo/didi-competition/tracklets/python
-cd ~/repo/CarND-Capstone
